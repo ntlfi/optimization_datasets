6e1984ad4324de8a51697ba3431cb4597aadbba2
Tianlong Nan <80225506+ntlfighting@users.noreply.github.com>
2024-08-30 13:57:19 -0400

    Updates

--- finance/stock/fetch.py
+++ finance/stock/fetch.py
@@ -6,16 +6,32 @@ import pandas as pd
 import yfinance as yf
 
 
-tickers_list = ['AAPL', 'WMT', 'IBM', 'MU', 'BA', 'AXP', 'GOOG', 'MSFT']
+tickers_list = ['AAPL', 'WMT', 'IBM', 'MU', 'BA', 'AXP', 'GOOG', 'MSFT', 'TSLA', 'AMZN', 'SOFI', 'PLTR', 'NIO', 'F', 'GM', 'T', 'VZ', 'NFLX', 'DIS', 'BABA', 'TCEHY', 'NVDA', 'AMD', 'INTC', 'QCOM', 'CSCO', 'ORCL', 'CRM', 'NOW', 'ZM', 'DOCU', 'UBER', 'LYFT', 'PINS', 'SNAP', 'META']
 data_list = []
+
 for ticker in tickers_list:
-    data = yf.download(ticker, period='1y')
+    data = yf.download(ticker, period='5y')
     data["ticker"] = ticker
     # Save the data
     data_list.append(data)
 data = pd.concat(data_list)
 
+# Reset the column names
+data = data.reset_index(drop=False)
+data = data.rename(columns={
+    'Date': 'date',
+    'Open': 'open',
+    'High': 'high',
+    'Low': 'low',
+    'Close': 'close',
+    'Adj Close': 'adj_close',
+    'Volume': 'volume'
+})
+
+data = data[['ticker', 'date', 'open', 'high', 'low', 'close', 'adj_close', 'volume']]
+
+
 # Save the data
 df = data
 
-df.to_csv('data.csv')
+df.to_csv('stock.csv')

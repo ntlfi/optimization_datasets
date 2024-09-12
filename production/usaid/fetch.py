ded89d935cb0759103508d7704a5479c98bf4eae
Tianlong Nan <80225506+ntlfighting@users.noreply.github.com>
2024-09-12 13:43:06 -0400

    Updates

---
+++ production/usaid/fetch.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sodapy import Socrata
+
+client = Socrata("data.usaid.gov", None)
+results = client.get("a3rc-nmf6", limit=3000, country="Vietnam")
+
+# Convert to pandas DataFrame
+df = pd.DataFrame.from_records(results)
+df.set_index('id', inplace=True)
+df = df[["country", "shipment_mode", "scheduled_delivery_date", "delivered_to_client_date", "product_group", "sub_classification", "unit_of_measure_per_pack", "line_item_quantity", "line_item_value", "pack_price", "unit_price", "weight_kilograms", "freight_cost_usd", "dosage"]]
+
+# Save the data to a CSV file
+df.to_csv('supply_chain_pricing.csv')
---
+++ production/usaid/problem.md
@@ -0,0 +1,8 @@
+### Problem A
+
+Given maximum weight capacity, maximize the total value of the items that can be carried.
+
+
+### Problem B
+
+Given required amount of items to be carried, minimize the freight cost.

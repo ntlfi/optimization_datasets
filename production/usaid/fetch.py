cbed744891ace9243b0eb8ca38f68b0c04fe296a
Tianlong Nan <ntlfighting@gmail.com>
2024-09-26 00:34:45 -0400

    Updates

--- production/usaid/fetch.py
+++ production/usaid/fetch.py
@@ -1,18 +1,25 @@
 import pandas as pd
 from sodapy import Socrata
+import os
 
 client = Socrata("data.usaid.gov", None)
-results = client.get("a3rc-nmf6", limit=3000, country="Vietnam")
+results = client.get("a3rc-nmf6", limit=30000)
 
 # Convert to pandas DataFrame
 df = pd.DataFrame.from_records(results)
 df.set_index('id', inplace=True)
 df = df[["country", "shipment_mode", "scheduled_delivery_date", "delivered_to_client_date", "product_group", "sub_classification", "unit_of_measure_per_pack", "line_item_quantity", "line_item_value", "pack_price", "unit_price", "weight_kilograms", "freight_cost_usd", "dosage"]]
+# Get the directory of the current file
+pwd = os.path.dirname(os.path.realpath(__file__))
 
 # Save the data to a CSV file
-df.to_csv('supply_chain_pricing.csv')
+df.to_csv(f'{pwd}/supply_chain_pricing.csv')
+
+
+
+
 
 countries = df['country'].unique()
-
 # Save country specific data to CSV files
-countries.to_csv('./airports/airports.csv')
+airports = pd.DataFrame({"country": countries})
+airports.to_csv(f'{pwd}/airports/airports.csv')
